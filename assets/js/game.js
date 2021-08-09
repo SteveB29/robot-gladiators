690468ddf396765531a783b4a6c059e919c285ab
Steve Bendrick <steven.bendrick@gmail.com>
2021-08-09 18:15:00 -0400

    added playerName variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! Javascript is running!");
+// This creates a funtion named "fight"
+
+var playerName = window.prompt("What is your robot's name?")
+console.log(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
