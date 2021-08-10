53d02859e720a8139db61d009003d4bfc2c0ef03
Steve Bendrick <steven.bendrick@gmail.com>
2021-08-09 22:01:43 -0400

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -52,7 +53,19 @@ var fight = function() {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
     } else if (promptFight === "skip" || promptFight === "SKIP") {
-        window.alert(playerName + " has chosen to skip the fight!");
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        // if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            // subtract mony from platerMoney for skipping
+            playerMoney = playerMoney - 2;
+        }
+        else {
+            // of no (false), ask question again by running fight() again
+            fight()
+        }
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
